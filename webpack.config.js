2f6fd6e152ce4ef68c6ff3a1898c2f3f966ca1b9
Haldun Bayhantopcu <hb@haldun.me>
2016-01-07 23:20:27 +0100

    init

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlwebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+
+const Target = process.env.npm_lifecycle_event;
+const Paths = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build'),
+};
+
+const common = {
+  entry: Paths.app,
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  output: {
+    path: Paths.build,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loaders: ['babel?cacheDirectory'],
+        include: Paths.app
+      }
+    ]
+  },
+  plugins: [
+    new HtmlwebpackPlugin({
+      title: 'My app'
+    })
+  ]
+};
+
+if (Target === 'start' || !Target) {
+  module.exports = merge(common, {
+    devServer: {
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
+      stats: 'errors-only',
+      host: process.env.HOST,
+      port: process.env.PORT
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ],
+    devtool: 'eval-source-map'
+  });
+}
+
+if (Target === 'build') {
+  module.exports = merge(common, {});
+}
